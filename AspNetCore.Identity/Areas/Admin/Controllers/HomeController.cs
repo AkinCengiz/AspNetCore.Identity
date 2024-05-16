39d2eabd7d2394570854bf9b656b3446abff8a33
akincengiz <akincengiz@outlook.com.tr>
2024-05-16 22:51:45 +0300

    Admin panel created

---
+++ AspNetCore.Identity/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AspNetCore.Identity.Areas.Admin.Models;
+using AspNetCore.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore.Identity.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class HomeController : Controller
+{
+	private readonly UserManager<AppUser> _userManager;
+
+	public HomeController(UserManager<AppUser> userManager)
+	{
+		_userManager = userManager;
+	}
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+	public async Task<IActionResult> UserList()
+	{
+		var userList = await _userManager.Users.ToListAsync();
+		var userViewModelList = userList.Select(x => new UserViewModel()
+		{
+			Id = x.Id,
+			Name = x.UserName,
+			Email = x.Email
+		}).ToList();
+		return View(userViewModelList);
+	}
+}
--- AspNetCore.Identity/Program.cs
+++ AspNetCore.Identity/Program.cs
@@ -35,6 +35,11 @@ public class Program
         app.UseAuthorization();
 
         app.MapControllerRoute(
+	        name: "areas",
+	        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+        );
+
+		app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
 
